5f3b6612ba38dc430248e2d73989c9cea02f07dd
Nicolas Pereira <41456803+hqnicolas@users.noreply.github.com>
2024-11-19 19:15:36 -0300

    New API Version

--- utils/response_writer.py
+++ utils/response_writer.py
@@ -8,17 +8,17 @@ class ResponseWriter:
         self._lock = threading.Lock()
 
     def normalize_text(self, text: str) -> str:
-        text = re.sub(r'/n`([abc123])', r'/n"\1', text)
-        text = re.sub(r'([abc123])` ', r'\1" ', text)
-        text = re.sub(r'`([abc123])', r'"\1', text)
-        text = re.sub(r'([abc123])`', r'\1"', text)
-        text = re.sub(r'`([abc123])', r'"\1', text)
-        text = re.sub(r'/n``', r'/n```', text)
         text = text.replace('\\"', '\\\" ')
         text = text.replace(':\"', ':\" ')
         text = text.replace('""""', '###')
-        text = text.replace('"""', '```')
         text = text.replace('":', '" :')
+        text = text.replace('**', '###')
+        text = text.replace('/n# ', '### ')
+        text = re.sub(r'(\w+)(?=\s\w)', r'\1` ', text)
+        text = re.sub(r'/n`', r'/n````', text)
+        text = re.sub(r'(`^`[]+)(?=\s.,!?[])', r'\1`', text)
+        text = re.sub(r'(\w+)\s(?=[^`])', r'\1` ', text)
+        text = re.sub(r'(\w+)\s`(\w+)(?=\s)', r'\1 `\2` ', text)
         return text
 
     def get_delta_content(self, current_message: str, threshold: float = 0.6) -> str:
@@ -36,9 +36,8 @@ class ResponseWriter:
                 
             delta = ""
             for tag, i1, i2, j1, j2 in sm.get_opcodes():
-                if tag in ('replace', 'insert'):
+                if tag in ("replace", 'insert'):
                     delta += current_message[j1:j2]
                     
             self.previous_message = current_message
             return delta
-
